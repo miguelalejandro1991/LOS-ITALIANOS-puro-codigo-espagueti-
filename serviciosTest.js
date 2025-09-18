9f959a4242dc120ec21d201a97a27d54555fb94a
miguelalejandro1991 <mibitech22@gmail.com>
2025-09-17 20:42:28 -0500

    saludo completo

--- serviciosTest.js
+++ serviciosTest.js
@@ -1,4 +1,34 @@
-generarSaludo=function(nombre,apellido){
-    return "Hola "+nombre+" "+apellido;
+generarSaludo=function(){
+    let nombre;
+    let apellido;
+    let resultado;
+    
+    nombre=recuperarTexto("Nombre");
+    apellido=recuperarTexto("Apellido");
+    resultado=mostrarTexto("lblResultado","Hola "+nombre+" "+apellido+" bienvenido al Reto Grupal");
+      
+   
+    
     
 }
+    
+
+recuperarTexto=function(idComponente){
+    let componente;
+    let valorIngresado;
+    componente=document.getElementById(idComponente);
+    valorIngresado=componente.value;
+    return valorIngresado;
+}
+mostrarTextoEnCaja=function(idComponente,mensaje){
+    let componente=document.getElementById(idComponente);
+    componente.value=mensaje;
+}
+mostrarTexto=function(idComponente,mensaje){
+    let componente=document.getElementById(idComponente);
+    componente.innerText=mensaje;       
+
+}
+
+    
+
